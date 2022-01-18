3a849c48a741da329bd001ddbbfbb888680a67bf
Andru Borisov <borisov21011986@rambler.ru>
2022-01-18 23:30:22 +0400

    Initial commit

---
+++ app/src/main/java/com/borisov/uiavtomator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.borisov.uiavtomator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
